82ee7cb27a2b21c9249313287f46cc0f1131c4cf
jai <jai.shiva4@gmail.com>
2020-04-10 20:34:51 +0530

    complete app

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/jaishiva/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/jaishiva/projects/flutter/Flutter-Destini"
+export "FLUTTER_TARGET=/Users/jaishiva/projects/flutter/Flutter-Destini/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/jaishiva/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
